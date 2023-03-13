0e91b4d580dfe7086267cbbbe8fa04282ffb18ef
Jenson Chun Hung Choi <61100042+JensonChoi@users.noreply.github.com>
2023-03-13 11:15:50 -0700

    Add href link to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,16 @@
-import React from "react";
-import '../styles/Button.css';
-import '../styles/global.css';
+import React from 'react'
+import '../styles/Button.css'
+import '../styles/global.css'
 
 function Button(props) {
-    return (
-        <button className="big-button">{props.name}</button>
-    );
+  function handleClick(e) {
+    window.location.href = 'https://moodify-backend1.herokuapp.com/login'
+  }
+  return (
+    <button className='big-button' onClick={handleClick}>
+      {props.name}
+    </button>
+  )
 }
 
-export default Button;
+export default Button
