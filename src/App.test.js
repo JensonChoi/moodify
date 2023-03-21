2c572bb1a53a5bb526038660db33dd84b5654d74
Jenson Chun Hung Choi <61100042+JensonChoi@users.noreply.github.com>
2023-03-20 23:35:52 -0700

    Fix unit test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
+import Home from './pages/Home'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('renders login button', () => {
+  render(<Home />)
+  const linkElement = screen.getByText(/login/i)
+  expect(linkElement).toBeInTheDocument()
+})
